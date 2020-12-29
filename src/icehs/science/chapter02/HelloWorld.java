03a966b0786b5ab70ac1bd8e8b570285adb74bb2
User <User@DESKTOP-L3RORA3>
2020-12-29 18:56:03 +0900

    my first

---
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -0,0 +1,9 @@
+package icehs.science.chapter02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.println("Hello World");
+	}
+}
