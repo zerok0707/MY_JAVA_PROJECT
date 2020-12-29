ca96a6e0784eb95dc08227a2662cec3eefacd4cb
User <User@DESKTOP-L3RORA3>
2020-12-29 20:33:05 +0900

    chapter03

---
+++ src/ichs/science/chapter03/TestScore.java
@@ -0,0 +1,18 @@
+package ichs.science.chapter03;
+
+public class TestScore {
+
+	public static void main( String[] arg) {
+	
+		int java;		
+		java = 100;	
+		int sql;		
+		sql= 80;		
+		int jsp = 85;	
+	
+		System.out.println( java );
+		System.out.println( sql );
+		System.out.println( jsp );
+	}
+
+}
