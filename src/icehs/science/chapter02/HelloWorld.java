0648798a135137728bc2d6524876057dec2cabf4
User <User@DESKTOP-L3RORA3>
2020-12-29 19:18:01 +0900

    seohyeonseok

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,7 +1,16 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
-
+	//동인천고등학교
+	//집주이수제
+	//응용프로그래밍개발
+	//서현석
+    /*
+     * Java Programming
+     * version 0.1
+     * 2020.12.29
+     * 
+     * */
 	public static void main(String[] args) {
 	// TODO Auto-generated method stub
 	System.out.println("Hello World!!!");
