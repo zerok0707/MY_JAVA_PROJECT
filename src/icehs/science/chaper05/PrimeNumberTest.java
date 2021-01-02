93e692202025a2d8fadaf07813161350e31b644f
User <User@DESKTOP-L3RORA3>
2021-01-02 15:51:54 +0900

    chapter05

--- src/icehs/science/chaper05/PrimeNumberTest.java
+++ src/icehs/science/chaper05/PrimeNumberTest.java
@@ -1,5 +1,18 @@
 package icehs.science.chaper05;
 
 public class PrimeNumberTest {
-
+	public static void main(String[] args) {
+		int firstNum = 23;
+		int secondNum = 26;
+		
+		for(int i = 2; i <= firstNum; i++) {
+			if(i != firstNum && firstNum % i == 0) {
+				System.out.println(firstNum + " 소수가 아닙니다.");
+			}else If(i == firstNum){
+				System.out.println();
+		}
+		for(int i = 2; i <= secondNum; i++) {
+			
+		}
+	}
 }
